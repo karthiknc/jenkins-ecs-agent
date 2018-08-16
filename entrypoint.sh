c782882f9eb85ce3d3b65d570027e9657e82fece
Karthik Bhat <karthik.bhat@news.co.uk>
2018-08-16 14:48:01 +0530

    Remove prepare creds

--- entrypoint.sh
+++ entrypoint.sh
@@ -104,8 +104,6 @@ if [[ ! -f /requirements.installed ]]; then
   touch /requirements.installed
 fi
 
-python /prepare-creds.py
-
 if [[ ! -z "$@" ]]; then
 	# If the user has given us a command, run it.
 	$@
