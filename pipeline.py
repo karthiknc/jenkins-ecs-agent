21e92eadb436f9dd3f50b9a2b73adecf0082d6dd
Karthik Bhat <karthik.bhat@news.co.uk>
2019-10-29 15:11:30 +0530

    Updated pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,20 +2,27 @@ import os
 import time
 import sys
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, NoCredentialsError
 
 
 class Pipeline:
     ARTIFACT_FILES = ['orchestrator.log']
     ENV_VARS = ['WORKFLOW', 'BUILD_ENV', 'SITE_REPO', 'SITE_BRANCH',
-                'GITHUB_TOKEN', 'DEPENDENCY_TAG', 'PLATFORM_BRANCH']
+                'GITHUB_TOKEN', 'DEPENDENCY_TAG', 'PLATFORM_BRANCH', 'BUILD_NUMBER']
 
     def __init__(self):
+        self.is_circle = 'CIRCLECI' in os.environ
         if 'WORKFLOW' not in os.environ:
-            print('Initial build. Exiting..')
-            exit()
+            if self.is_circle:
+                os.environ['WORKFLOW'] = 'build-wp-platform' \
+                    if os.environ['CIRCLE_PROJECT_REPONAME'] == 'nu-ecsplatform' else 'build-deploy-site'
+            else:
+                print('Initial build. Exiting..')
+                exit()
 
         self.session = None
+        if self.is_circle:
+            self.prepare_circle_envs()
         profile = 'prod' if os.environ['BUILD_ENV'] in ('staging', 'prod') else 'dev'
         self.client = self._get_codebuild_client(profile)
         self.build_kwargs = {}
@@ -23,21 +30,44 @@ class Pipeline:
         if len(sys.argv) > 1:
             self.codebuild_project = sys.argv[1]
 
+    def prepare_circle_envs(self):
+        if os.environ['CIRCLE_BRANCH'] == 'master':
+            os.environ['BUILD_ENV'] = 'prod'
+        else:
+            os.environ['BUILD_ENV'] = os.environ['CIRCLE_BRANCH']
+
+        if 'DEPENDENCY_TAG' not in os.environ:
+            os.environ['DEPENDENCY_TAG'] = 'latest'
+
+        if 'PLATFORM_BRANCH' not in os.environ:
+            os.environ['PLATFORM_BRANCH'] = 'master'
+
+        if 'SITE_BRANCH' not in os.environ:
+            os.environ['SITE_BRANCH'] = 'master'
+
     def _get_codebuild_client(self, profile):
         roles = {
             'dev': 'arn:aws:iam::709143057981:role/CloudFusionCDRole',
             'prod': 'arn:aws:iam::731530244584:role/CloudFusionCDRole'
         }
-        sts = boto3.client('sts')
-        credentials = sts.assume_role(
-            RoleArn=roles[profile],
-            RoleSessionName='jenkins'
-        )['Credentials']
-        self.session = boto3.session.Session(
-            aws_access_key_id=credentials['AccessKeyId'],
-            aws_secret_access_key=credentials['SecretAccessKey'],
-            aws_session_token=credentials['SessionToken']
-        )
+
+        try:
+            sts = boto3.client('sts')
+            credentials = sts.assume_role(
+                RoleArn=roles[profile],
+                RoleSessionName='jenkins'
+            )['Credentials']
+            self.session = boto3.session.Session(
+                aws_access_key_id=credentials['AccessKeyId'],
+                aws_secret_access_key=credentials['SecretAccessKey'],
+                aws_session_token=credentials['SessionToken']
+            )
+        except NoCredentialsError as e:
+            print('No credentials error: {}'.format(e))
+            self.session = boto3.session.Session(
+                aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+            )
         return self.session.client('codebuild')
 
     def prepare(self):
@@ -54,6 +84,16 @@ class Pipeline:
                     'name': 'SITE_REPO',
                     'value': os.environ['GIT_URL'].split('/')[-1].split('.')[0]
                 })
+            elif env_var == 'SITE_REPO' and 'CIRCLE_PROJECT_REPONAME' in os.environ:
+                env_vars.append({
+                    'name': 'SITE_REPO',
+                    'value': os.environ['CIRCLE_PROJECT_REPONAME']
+                })
+            if env_var == 'BUILD_NUMBER' and self.is_circle:
+                env_vars.append({
+                    'name': 'BUILD_NUMBER',
+                    'value': os.environ['CIRCLE_BUILD_NUM']
+                })
 
         self.build_kwargs = {
             'projectName': self.codebuild_project,
@@ -63,6 +103,10 @@ class Pipeline:
 
     def run_build(self):
         print('Build starting..')
+
+        if os.environ['BUILD_ENV'] == 'prod':
+            print('Please trigger the build manually for production environment.')
+            return False
 
         builder_run = self.client.start_build(**self.build_kwargs)
         build_id = builder_run['build']['id']
@@ -115,4 +159,5 @@ if __name__ == '__main__':
     pl = Pipeline()
     pl.prepare()
     bid = pl.run_build()
-    pl.manage_artifacts(bid)
+    if bid:
+        pl.manage_artifacts(bid)
